b25f8482bd6371d075c9ce7b00a6148ee4baacc8
alf <alf@akademe.net>
2015-08-20 14:06:17 +0200

    added more check so nothing below One minute

--- pomo.js
+++ pomo.js
@@ -4,7 +4,7 @@ var activeTimer = workTime; // initial timer to run
 var running = false, onBreak = false;
 var ticker;
 
-var audio = new Audio('Ship_Bell-Mike_Koenig-1911209136.mp3');
+var alarm = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/326176/Ship_Bell-Mike_Koenig-1911209136.mp3');
 
 $(document).ready(function() {
 
@@ -15,39 +15,23 @@ $(document).ready(function() {
   $('#start').click(function()
   {
     if (!running) {
-      /* Disable all buttons except stop, avoid tinkering while enabled*/
+      /* Disable - and + buttons */
       $('.set-time').prop('disabled', true);
-      $('#reset').prop('disabled', true);
-
+      $('#togglebutton').html('&nbsp;&nbsp;Pause&nbsp;&nbsp;');
       /* start timer and change state */
       ticker = setInterval(tick, 1000);
+      running = !running;
+    } else {
+      $('.set-time').prop('disabled', false);
+      $('#togglebutton').html('Continue');
+      clearInterval(ticker);
       running = !running;
     }
     $(this).blur();	//remove focus from button.
   });
 
-  $('#stop').click(function() {
-    if (running) {
-      /* Enable some buttons  */
-      $('.set-time').prop('disabled', false);
-      $('#reset').prop('disabled', false);
-
-      clearInterval(ticker);
-      running = !running;
-    }
-    $(this).blur();
-  });
-
-  $('#reset').click(function() {
-
-    $('#busytime').html(displayTimer(workTime));
-    activeTimer = workTime;
-
-    $(this).blur();
-  });
-
-  /* Change work length */
-  $('#m-plus').click(function() {
+  /* Change work time */
+  $('#increase-work').click(function() {
     workTime += 60;
 
     if (!onBreak) {
@@ -56,30 +40,25 @@ $(document).ready(function() {
     } else {
       $('#busytime').html(displayTimer(workTime));
     }
-
-    $(this).blur();
   });
 
-  $('#m-min').click(function() {
+  $('#decrease-work').click(function() {
     if (workTime >= 60) {
       workTime -= 60;
 
-      if (!onBreak) {
+      if (!onBreak && activeTimer >= 60) {
         activeTimer -= 60;
         $('#busytime').html(displayTimer(activeTimer));
+      } else if (!onBreak && activeTimer < 60) {
+          $('#busytime').html(displayTimer(activeTimer));
       } else {
         $('#busytime').html(displayTimer(workTime));
       }
-
-      // $('#busytime').html(displayTimer(timer));
     }
-    $(this).blur();
   });
 
-  /* Change break length */
-
-  $('#break-m-plus').click(function() {
-    /* Add sixty seconds to breakTime */
+  /* Change break time */
+  $('#increase-break').click(function() {
     breakTime += 60;
 
     if (onBreak) {
@@ -88,25 +67,25 @@ $(document).ready(function() {
     } else {
       $('#breaktime').html(displayTimer(breakTime));
     }
-    this.blur();
   });
 
-  $('#break-m-min').click(function() {
+  $('#decrease-break').click(function() {
     if (breakTime >= 60)
       breakTime -= 60;
 
-    if (onBreak) {
+    if (onBreak && activeTimer >= 60) {
       activeTimer -= 60;
       $('#breaktime').html(displayTimer(activeTimer));
+    }  else if (onBreak && activeTimer < 60) {
+        $('#breaktime').html(displayTimer(activeTimer));
     } else {
       $('#breaktime').html(displayTimer(breakTime));
     }
-    this.blur();
   });
 });
 
 function tick() {
-  /* Whenever there's time on the clock we deduct a second */
+  /* deduct a second */
   if (activeTimer > 0) {
 
     if (onBreak)
@@ -116,24 +95,20 @@ function tick() {
 
     activeTimer--;
   }
-
   /* When timer = 0, alarm is played */
-
   if (activeTimer === 0) {
-    audio.volume = 1;
-    audio.play();
+    alarm.volume = 1;
+    alarm.play();
     clearInterval(ticker);
 
     if (onBreak) {
       activeTimer = workTime;
       onBreak = false;
-
       $('#busytime').html(displayTimer(workTime));
       $('#breaktime').html(displayTimer(breakTime));
     } else {
       activeTimer = breakTime;
       onBreak = true;
-
       $('#breaktime').html(displayTimer(breakTime));
     }
     /* Restart the timer in the next part of the sequence */
